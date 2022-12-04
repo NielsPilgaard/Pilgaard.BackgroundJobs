31e1cc5530fc837f6edaf5d894d36b41354b251b
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2022-12-04 22:14:21 +0100

    added slow cron job to illustrate that tags help differentiate metrics

---
+++ samples/Pilgaard.CronJobs.Examples.OpenTelemetry/SlowCronJob.cs
@@ -0,0 +1,27 @@
+using Cronos;
+
+namespace Pilgaard.CronJobs.Examples.OpenTelemetry;
+
+public class SlowCronJob : ICronJob
+{
+    private readonly ILogger<SlowCronJob> _logger;
+
+    public SlowCronJob(ILogger<SlowCronJob> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task ExecuteAsync(CancellationToken cancellationToken = default)
+    {
+        _logger.LogInformation("Executing CronJob {nameofApiCallerCronJob} at {timeNow}",
+            nameof(SlowCronJob),
+            DateTime.UtcNow.ToString("T"));
+
+        await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+    }
+
+    /// <summary>
+    /// Executes once every second
+    /// </summary>
+    public CronExpression CronSchedule => CronExpression.Parse("* * * * * *", CronFormat.IncludeSeconds);
+}
