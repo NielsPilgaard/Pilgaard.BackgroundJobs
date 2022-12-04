ea358ec3b8b4421b97b96ca3024fdbfa81fa8fa9
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2022-12-04 21:52:58 +0100

    added helpers for using System.Diagnostics.Metrics Histogram to measure duration

---
+++ src/Pilgaard.CronJobs/Extensions/HistogramExtensions.cs
@@ -0,0 +1,16 @@
+using System.Diagnostics.Metrics;
+using Pilgaard.CronJobs.Telemetry;
+using Timer = Pilgaard.CronJobs.Telemetry.Timer;
+
+namespace Pilgaard.CronJobs.Extensions;
+
+public static class HistogramExtensions
+{
+    /// <summary>
+    /// Enables you to easily report elapsed seconds in the value of a <see cref="Histogram{T}"/>.
+    /// <para>
+    /// Dispose of the returned instance to report the elapsed duration.
+    /// </para>
+    /// </summary>
+    public static ITimer NewTimer(this Histogram<double> histogram) => new Timer(histogram);
+}
