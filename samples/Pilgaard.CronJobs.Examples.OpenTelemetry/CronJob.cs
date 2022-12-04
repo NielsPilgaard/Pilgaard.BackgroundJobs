69243bef9c791e1d6e064bc58b4971451c758bb1
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2022-12-04 21:51:45 +0100

    add open-telemetry sample project

---
+++ samples/Pilgaard.CronJobs.Examples.OpenTelemetry/CronJob.cs
@@ -0,0 +1,30 @@
+using Cronos;
+
+namespace Pilgaard.CronJobs.Examples.OpenTelemetry;
+
+public class CronJob : ICronJob
+{
+    private readonly ILogger<CronJob> _logger;
+    private static readonly HttpClient _client = new();
+
+    public CronJob(ILogger<CronJob> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task ExecuteAsync(CancellationToken cancellationToken = default)
+    {
+        _logger.LogInformation("Executing CronJob {nameofApiCallerCronJob} at {timeNow}",
+            nameof(CronJob),
+            DateTime.UtcNow.ToString("T"));
+
+        const string endpoint = "https://localhost:7243/weatherforecast";
+
+        await _client.GetAsync(endpoint, cancellationToken);
+    }
+
+    /// <summary>
+    /// Executes once every second
+    /// </summary>
+    public CronExpression CronSchedule => CronExpression.Parse("* * * * * *", CronFormat.IncludeSeconds);
+}
