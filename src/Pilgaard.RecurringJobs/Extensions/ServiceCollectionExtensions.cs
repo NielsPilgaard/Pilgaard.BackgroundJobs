3bc55d3d67a04cb613183553cf7b07be7d682a1e
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2022-12-30 19:23:51 +0100

    cleanup after removing options.

--- src/Pilgaard.RecurringJobs/Extensions/ServiceCollectionExtensions.cs
+++ src/Pilgaard.RecurringJobs/Extensions/ServiceCollectionExtensions.cs
@@ -1,10 +1,8 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Pilgaard.RecurringJobs.Configuration;
 
 namespace Pilgaard.RecurringJobs.Extensions;
 
@@ -25,7 +23,7 @@ public static class ServiceCollectionExtensions
 #endif
     public static IServiceCollection AddRecurringJobs(this IServiceCollection services, params Type[] types)
     {
-        return services.AddRecurringJobs(types.Select(type => type.GetTypeInfo().Assembly), null);
+        return services.AddRecurringJobs(types.Select(type => type.GetTypeInfo().Assembly));
     }
 
     /// <summary>
@@ -43,26 +41,7 @@ public static class ServiceCollectionExtensions
 #endif
     public static IServiceCollection AddRecurringJobs(this IServiceCollection services, params Assembly[] assembliesToScan)
     {
-        return services.AddRecurringJobs(assembliesToScan, null);
-    }
-
-    /// <summary>
-    ///     Finds and adds all <see cref="IRecurringJob"/>s to the <see cref="IServiceCollection"/>.
-    /// <para>
-    ///     Each <see cref="IRecurringJob"/> is then hosted in a <see cref="RecurringJobBackgroundService"/>.
-    /// </para>
-    /// </summary>
-    /// <param name="services">The service collection.</param>
-    /// <param name="types">The types to scan for <see cref="IRecurringJob"/>s through.</param>
-    /// <param name="configuration">The configurator of <see cref="RecurringJobOptions"/>.</param>
-    /// <returns>The <see cref="IServiceCollection"/> for further chaining.</returns>
-    /// <exception cref="ArgumentException">No assemblies found to scan. Supply at least one assembly to scan for Cron Services.</exception>
-#if NET6_0_OR_GREATER
-    [RequiresUnreferencedCode("Calls System.Reflection.Assembly.ExportedTypes")]
-#endif
-    public static IServiceCollection AddRecurringJobs(this IServiceCollection services, Action<RecurringJobOptions>? configuration = null, params Type[] types)
-    {
-        return services.AddRecurringJobs(types.Select(type => type.GetTypeInfo().Assembly), configuration);
+        return services.AddRecurringJobs(assembliesToScan.AsEnumerable());
     }
 
     /// <summary>
@@ -73,45 +52,18 @@ public static class ServiceCollectionExtensions
     /// </summary>
     /// <param name="services">The service collection.</param>
     /// <param name="assembliesToScan">The assemblies to scan for <see cref="IRecurringJob"/>s.</param>
-    /// <param name="configuration">The configurator of <see cref="RecurringJobOptions"/>.</param>
-    /// <returns>The <see cref="IServiceCollection"/> for further chaining.</returns>
-    /// <exception cref="ArgumentException">No assemblies found to scan. Supply at least one assembly to scan for Cron Services.</exception>
-#if NET6_0_OR_GREATER
-    [RequiresUnreferencedCode("Calls System.Reflection.Assembly.ExportedTypes")]
-#endif
-    public static IServiceCollection AddRecurringJobs(this IServiceCollection services, Action<RecurringJobOptions>? configuration = null, params Assembly[] assembliesToScan)
-    {
-        return services.AddRecurringJobs(assembliesToScan, configuration);
-    }
-
-    /// <summary>
-    ///     Finds and adds all <see cref="IRecurringJob"/>s to the <see cref="IServiceCollection"/>.
-    /// <para>
-    ///     Each <see cref="IRecurringJob"/> is then hosted in a <see cref="RecurringJobBackgroundService"/>.
-    /// </para>
-    /// </summary>
-    /// <param name="services">The service collection.</param>
-    /// <param name="assembliesToScan">The assemblies to scan for <see cref="IRecurringJob"/>s.</param>
-    /// <param name="configurationAction">The configurator of <see cref="RecurringJobOptions"/>.</param>
     /// <returns>The <see cref="IServiceCollection"/> for further chaining.</returns>
     /// <exception cref="ArgumentException">No assemblies found to scan. Supply at least one assembly to scan for Cron Services.</exception>
 #if NET6_0_OR_GREATER
     [RequiresUnreferencedCode("Calls System.Reflection.Assembly.ExportedTypes")]
 #endif
     private static IServiceCollection AddRecurringJobs(this IServiceCollection services,
-    IEnumerable<Assembly> assembliesToScan,
-        Action<RecurringJobOptions>? configurationAction)
+    IEnumerable<Assembly> assembliesToScan)
     {
         if (!assembliesToScan.Any())
         {
             throw new ArgumentException("No assemblies found to scan. Supply at least one assembly to scan for Cron Services.");
         }
-
-        var options = new RecurringJobOptions();
-
-        configurationAction?.Invoke(options);
-
-        services.TryAddSingleton(options);
 
         foreach (var assembly in assembliesToScan)
         {
@@ -121,7 +73,7 @@ public static class ServiceCollectionExtensions
 
             foreach (var job in implementsICronJob)
             {
-                RegisterJob(services, options.ServiceLifetime, job);
+                RegisterJob(services, job);
                 AddHostedService(services, job);
             }
         }
@@ -133,10 +85,8 @@ public static class ServiceCollectionExtensions
     /// Registers the cron job through a <see cref="ServiceDescriptor"/>.
     /// </summary>
     /// <param name="services">The service collection.</param>
-    /// <param name="serviceLifetime">The service life time.</param>
     /// <param name="concreteClass">The concrete class.</param>
     private static void RegisterJob(IServiceCollection services,
-        ServiceLifetime serviceLifetime,
 #if NET7_0_OR_GREATER
         [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] Type concreteClass
 #endif
@@ -148,12 +98,12 @@ public static class ServiceCollectionExtensions
         services.Add(new ServiceDescriptor(
             typeof(IRecurringJob),
             concreteClass,
-            serviceLifetime));
+            ServiceLifetime.Transient));
 
         services.Add(new ServiceDescriptor(
             concreteClass,
             concreteClass,
-            serviceLifetime));
+            ServiceLifetime.Transient));
     }
 
     /// <summary>
@@ -168,7 +118,6 @@ public static class ServiceCollectionExtensions
         services.AddSingleton<IHostedService>(provider =>
             new RecurringJobBackgroundService((IRecurringJob)provider.GetRequiredService(@class),
                 provider.GetRequiredService<IServiceScopeFactory>(),
-                provider.GetRequiredService<ILogger<RecurringJobBackgroundService>>(),
-                provider.GetRequiredService<RecurringJobOptions>()));
+                provider.GetRequiredService<ILogger<RecurringJobBackgroundService>>()));
     }
 }
