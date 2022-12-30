ba0bd0668c9c9b7bc9980b01d4cc542738d84885
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2022-12-30 19:24:44 +0100

    event based async timer implementation, since it needs to work on netstandard

--- src/Pilgaard.RecurringJobs/RecurringJobBackgroundService.cs
+++ src/Pilgaard.RecurringJobs/RecurringJobBackgroundService.cs
@@ -2,7 +2,6 @@ using System.Diagnostics.Metrics;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Pilgaard.RecurringJobs.Configuration;
 using Pilgaard.RecurringJobs.Extensions;
 
 namespace Pilgaard.RecurringJobs;
@@ -12,7 +11,7 @@ namespace Pilgaard.RecurringJobs;
 /// by hosting it as a <see cref="BackgroundService"/>,
 /// <para>
 /// The <see cref="RecurringJobBackgroundService"/> runs <see cref="IRecurringJob.ExecuteAsync"/>
-/// whenever <see cref="IRecurringJob.CronSchedule"/> triggers.
+/// whenever <see cref="IRecurringJob.Interval"/> time has elapsed.
 /// </para>
 /// </summary>
 /// <remarks>
@@ -21,75 +20,67 @@ namespace Pilgaard.RecurringJobs;
 public class RecurringJobBackgroundService : BackgroundService
 {
     private readonly IServiceScopeFactory _serviceScopeFactory;
-    private readonly IRecurringJob _cronJob;
+    private readonly IRecurringJob _job;
     private readonly ILogger<RecurringJobBackgroundService> _logger;
-    private readonly RecurringJobOptions _options;
     private readonly string _jobName;
+
+    private Timer? _timer;
+
+    private event Func<object, EventArgs, CancellationToken, Task>? TimerTriggered;
 
     private static readonly Meter _meter = new(
         name: typeof(RecurringJobBackgroundService).Assembly.GetName().Name!,
         version: typeof(RecurringJobBackgroundService).Assembly.GetName().Version?.ToString());
 
     private static readonly Histogram<double> _histogram =
-        _meter.CreateHistogram<double>("cronjob.executeasync",
-            "milliseconds",
-            "Histogram over duration and count of IRecurringJob.ExecuteAsync.");
+        _meter.CreateHistogram<double>(
+            name: $"{nameof(IRecurringJob)}.{nameof(ExecuteAsync)}".ToLower(),
+            unit: "milliseconds",
+            description: $"Histogram over duration and count of {nameof(IRecurringJob)}.{nameof(IRecurringJob.ExecuteAsync)}.");
 
     /// <summary>
     /// Initializes a new instance of the <see cref="RecurringJobBackgroundService"/> class.
     /// </summary>
-    /// <param name="cronJob">The cron job.</param>
+    /// <param name="job">The job.</param>
     /// <param name="serviceScopeFactory">The service scope factory.</param>
     /// <param name="logger">The logger.</param>
-    /// <param name="options">The options.</param>
     public RecurringJobBackgroundService(
-        IRecurringJob cronJob,
+        IRecurringJob job,
         IServiceScopeFactory serviceScopeFactory,
-        ILogger<RecurringJobBackgroundService> logger,
-        RecurringJobOptions options)
+        ILogger<RecurringJobBackgroundService> logger)
     {
-        _options = options;
         _serviceScopeFactory = serviceScopeFactory;
         _logger = logger;
-        _cronJob = cronJob;
-        _jobName = _cronJob.GetType().Name;
+        _job = job;
+        _jobName = _job.GetType().Name;
 
-        _logger.LogInformation("Started CronBackgroundService with CronJob {cronJobName}", _jobName);
+        _logger.LogInformation("Started {className} with Job {job}",
+            nameof(RecurringJobBackgroundService), _jobName);
     }
-
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        var nextTaskOccurrence = GetNextOccurrence();
+        _timer = new Timer(_ => TimerTriggered?.Invoke(this, EventArgs.Empty, stoppingToken),
+           state: null,
+           dueTime: _job.InitialDelay,
+           period: _job.Interval);
 
-        while (nextTaskOccurrence is not null &&
-               stoppingToken.IsCancellationRequested is false)
-        {
-            _logger.LogDebug("The next time {cronJobName} will execute is {nextTaskOccurrence}", _jobName, nextTaskOccurrence);
+        TimerTriggered += InternalExecuteAsync;
 
-            await PerformTaskOnNextOccurrenceAsync(nextTaskOccurrence, stoppingToken);
-
-            nextTaskOccurrence = GetNextOccurrence();
-        }
+        return Task.CompletedTask;
     }
 
     /// <summary>
-    /// Performs the task on next occurrence.
+    /// Performs the <see cref="IRecurringJob.ExecuteAsync"/> task.
     /// </summary>
-    /// <param name="nextTaskExecution">The next task execution.</param>
+    /// <param name="sender"></param>
+    /// <param name="eventArgs"></param>
     /// <param name="stoppingToken">The stopping token.</param>
-    private async Task PerformTaskOnNextOccurrenceAsync(
-        DateTime? nextTaskExecution,
-        CancellationToken stoppingToken)
+    internal async Task InternalExecuteAsync(object sender, EventArgs eventArgs, CancellationToken stoppingToken)
     {
-        // If UtcNow is higher than the time to execute next, we've already passed it
-        if (NextOccurrenceIsInThePast(nextTaskExecution))
+        if (stoppingToken.IsCancellationRequested)
         {
             return;
         }
-
-        var delay = TimeUntilNextOccurrence(nextTaskExecution);
-
-        await Task.Delay(delay, stoppingToken);
 
         // Measure duration of ExecuteAsync
         using var timer = _histogram.NewTimer(tags:
@@ -99,66 +90,34 @@ public class RecurringJobBackgroundService : BackgroundService
 
         // If ServiceLifetime is Transient or Scoped, we need to re-fetch the
         // CronJob from the ServiceProvider on every execution.
-        if (_options.ServiceLifetime is not ServiceLifetime.Singleton)
+        if (_job.ServiceLifetime is not ServiceLifetime.Singleton)
         {
-            await GetScopedJobAndExecuteAsync(stoppingToken);
+            _logger.LogDebug(
+                "Fetching a {serviceLifetime} instance of {jobName} from the IServiceScopeFactory.",
+                _job.ServiceLifetime, _jobName);
+
+            using var scope = _serviceScopeFactory.CreateScope();
+
+            var job = (IRecurringJob)scope.ServiceProvider.GetRequiredService(_job.GetType());
+
+            await job.ExecuteAsync(stoppingToken).ConfigureAwait(false);
+
+            _logger.LogDebug("Successfully executed the Job {jobName}", _jobName);
+
             return;
         }
 
-        await _cronJob.ExecuteAsync(stoppingToken);
+        await _job.ExecuteAsync(stoppingToken);
     }
 
-    /// <summary>
-    ///     Gets the <see cref="DateTime"/> of the next time
-    ///     <see cref="IRecurringJob.ExecuteAsync"/> should trigger.
-    /// </summary>
-    /// <returns>
-    ///     The <see cref="DateTime"/> of the next time
-    ///     <see cref="IRecurringJob.ExecuteAsync"/> should trigger.
-    /// </returns>
-    private DateTime? GetNextOccurrence()
-        => DateTime.MaxValue;
-
-    /// <summary>
-    ///     Gets the scoped <see cref="IRecurringJob"/> and executes it.
-    /// </summary>
-    /// <param name="stoppingToken">The stopping token.</param>
-    private async Task GetScopedJobAndExecuteAsync(CancellationToken stoppingToken)
+    public override void Dispose()
     {
-        _logger.LogDebug(
-            "Fetching a {serviceLifetime} instance of {cronJobName} from the ServiceProvider.",
-            _options.ServiceLifetime, _jobName);
+        _timer?.Dispose();
 
-        using var scope = _serviceScopeFactory.CreateScope();
+        TimerTriggered -= InternalExecuteAsync;
 
-        var cronJob = (IRecurringJob)scope.ServiceProvider.GetService(_cronJob.GetType())!;
+        GC.SuppressFinalize(this);
 
-        await cronJob.ExecuteAsync(stoppingToken);
-
-        _logger.LogDebug("Successfully executed the CronJob {cronJobName}", _jobName);
+        base.Dispose();
     }
-
-    /// <summary>
-    ///     Checks whether the <paramref name="nextTaskExecution"/> is before
-    ///     <see cref="DateTime.UtcNow"/>.
-    /// </summary>
-    /// <param name="nextTaskExecution">The next task execution.</param>
-    /// <returns>
-    ///     <c>true</c> if <paramref name="nextTaskExecution"/>
-    ///     is before <see cref="DateTime.UtcNow"/>, otherwise <c>false</c>
-    /// </returns>
-    private static bool NextOccurrenceIsInThePast(DateTime? nextTaskExecution)
-        => DateTime.UtcNow > nextTaskExecution.GetValueOrDefault();
-
-    /// <summary>
-    ///     Gets the <see cref="TimeSpan"/> until the next
-    ///     <see cref="IRecurringJob.ExecuteAsync"/> should be triggered.
-    /// </summary>
-    /// <param name="nextTaskExecutionTime">The next task execution.</param>
-    /// <returns>
-    ///     The <see cref="TimeSpan"/> until the next
-    ///     <see cref="IRecurringJob.ExecuteAsync"/> should be triggered.
-    /// </returns>
-    private static TimeSpan TimeUntilNextOccurrence(DateTime? nextTaskExecutionTime)
-        => nextTaskExecutionTime.GetValueOrDefault() - DateTime.UtcNow;
 }
