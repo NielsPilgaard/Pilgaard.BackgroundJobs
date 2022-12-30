8bab1a71a3772787b0157f2502a665c9e9b7e69f
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2022-12-30 22:43:59 +0100

    added api approval tests, mostly copied from aspnetcore.healthchecks

---
+++ tests/ApiApprovalTests.cs
@@ -0,0 +1,42 @@
+using System.Diagnostics;
+using System.Reflection;
+using PublicApiGenerator;
+using Shouldly;
+
+/// <see href="https://github.com/JakeGinnivan/ApiApprover"/>
+public class ApiApprovalTests
+{
+    [Fact]
+    [Trait("Category", "API Approval")]
+    public void public_api_should_not_change_unintentionally()
+    {
+        var executingAssembly = Assembly.GetExecutingAssembly();
+        var dependencies = executingAssembly.GetReferencedAssemblies();
+        string nameToFind = executingAssembly.GetName().Name!.Split('.')[1]; // 0 is always "Pilgaard"
+
+        var assemblyToTest = dependencies.Select(Assembly.Load)
+            .Where(assembly => assembly.GetTypes().Any(type => type.Name == "ApiMarker") &&
+                               assembly.GetName().Name!.Contains(nameToFind, StringComparison.InvariantCultureIgnoreCase))
+            .Single();
+
+        string publicApi = assemblyToTest.GeneratePublicApi(new ApiGeneratorOptions
+        {
+            IncludeAssemblyAttributes = false,
+            WhitelistedNamespacePrefixes = new[] { "Microsoft.Extensions.DependencyInjection" }
+        });
+
+        string location = Assembly.GetExecutingAssembly().Location;
+        string[] pathItems = location.Split(Path.DirectorySeparatorChar);
+        int index = Array.IndexOf(pathItems, "test");
+        Debug.Assert(index > 0 && index < pathItems.Length - 1);
+
+        // See: https://shouldly.readthedocs.io/en/latest/assertions/shouldMatchApproved.html
+        // Note: If the AssemblyName.approved.txt file doesn't match the latest publicApi value,
+        // this call will try to launch a diff tool to help you out but that can fail on
+        // your machine if a diff tool isn't configured/setup.
+        publicApi.ShouldMatchApproved(options =>
+            options.SubFolder(pathItems[index + 1])
+                .WithFilenameGenerator((_, _, fileType, fileExtension)
+                    => $"{assemblyToTest.GetName().Name}.{fileType}.{fileExtension}"));
+    }
+}
