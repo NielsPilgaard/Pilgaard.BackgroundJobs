9dbeb945b3c0c9865584d1e4c252fea4a4f5767a
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-01-14 23:06:56 +0100

    Create BackgroundJobOccurrence.cs

---
+++ src/Pilgaard.BackgroundJobs/BackgroundJobOccurrence.cs
@@ -0,0 +1,7 @@
+namespace Pilgaard.BackgroundJobs;
+
+internal readonly record struct BackgroundJobOccurrence(DateTime? Occurrence, IBackgroundJob BackgroundJob)
+{
+    public DateTime? Occurrence { get; } = Occurrence;
+    public IBackgroundJob BackgroundJob { get; } = BackgroundJob;
+}
