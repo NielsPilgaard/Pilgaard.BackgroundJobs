b8da94c48a0d26310e7372c7ad101073a9124aef
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-01-21 20:40:48 +0100

    added async jobs

--- samples/Pilgaard.BackgroundJobs.Examples.MinimalAPI/Program.cs
+++ samples/Pilgaard.BackgroundJobs.Examples.MinimalAPI/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddBackgroundJobs()
     .AddJob(
         name: "basic-cronjob",
         job: () => Console.WriteLine("I happened!"),
-        cronExpression: CronExpression.Parse("* * * * *", CronFormat.IncludeSeconds))
+        cronExpression: CronExpression.Parse("* * * * *"))
     .AddAsyncJob(
         name: "async-recurringjob",
         job: async (cancellationToken) => await new HttpClient().GetStringAsync("https://localhost:7149/weatherforecast", cancellationToken: cancellationToken),
@@ -17,7 +17,20 @@ builder.Services.AddBackgroundJobs()
         name: "basic-onetimejob",
         job: () => Console.WriteLine($"Triggered at {DateTime.Now}"),
         scheduledTimeUtc: DateTime.UtcNow.AddHours(1))
-    .AddJob<CronJob>(name: nameof(CronJob));
+    .AddJob<CronJob>(name: nameof(CronJob))
+    .AddAsyncJob(
+        name: "async-cronjob",
+        job: cancellationToken => Task.CompletedTask,
+        cronExpression: CronExpression.Parse("* * * * *"))
+    .AddAsyncJob(
+        name: "async-recurringjob",
+        job: cancellationToken => Task.CompletedTask,
+        interval: TimeSpan.FromSeconds(3))
+    .AddAsyncJob(
+        name: "async-onetimejob",
+        job: cancellationToken => Task.CompletedTask,
+        scheduledTimeUtc: DateTime.UtcNow.AddHours(1));
+;
 
 var app = builder.Build();
 
