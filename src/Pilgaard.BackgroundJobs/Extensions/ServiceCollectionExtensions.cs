ee2987980d6707cf36e2de35837cc3d05160adda
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-01-21 20:05:35 +0100

    add DefaultBackgroundJobService as HostedService

--- src/Pilgaard.BackgroundJobs/Extensions/ServiceCollectionExtensions.cs
+++ src/Pilgaard.BackgroundJobs/Extensions/ServiceCollectionExtensions.cs
@@ -8,6 +8,7 @@ public static class ServiceCollectionExtensions
     public static IBackgroundJobsBuilder AddBackgroundJobs(this IServiceCollection services)
     {
         services.TryAddSingleton<IBackgroundJobService, DefaultBackgroundJobService>();
+        services.AddHostedService<DefaultBackgroundJobService>();
         services.TryAddSingleton<IBackgroundJobScheduler, BackgroundJobScheduler>();
         return new BackgroundJobsBuilder(services);
     }
