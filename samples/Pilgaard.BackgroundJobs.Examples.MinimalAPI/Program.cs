0f8ede1f124cd37450acc06af2eeff68e49bac22
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-01-21 20:20:45 +0100

    extended on the minimalAPI sample

--- samples/Pilgaard.BackgroundJobs.Examples.MinimalAPI/Program.cs
+++ samples/Pilgaard.BackgroundJobs.Examples.MinimalAPI/Program.cs
@@ -1,8 +1,23 @@
+using Cronos;
 using Pilgaard.BackgroundJobs.Examples.MinimalAPI;
+using Pilgaard.BackgroundJobs.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddBackgroundJobs()
+    .AddJob(
+        name: "basic-cronjob",
+        job: () => Console.WriteLine("I happened!"),
+        cronExpression: CronExpression.Parse("* * * * *", CronFormat.IncludeSeconds))
+    .AddAsyncJob(
+        name: "async-recurringjob",
+        job: async (cancellationToken) => await new HttpClient().GetStringAsync("https://localhost:7149/weatherforecast", cancellationToken: cancellationToken),
+        interval: TimeSpan.FromSeconds(3))
+    .AddJob(
+        name: "basic-onetimejob",
+        job: () => Console.WriteLine($"Triggered at {DateTime.Now}"),
+        scheduledTimeUtc: DateTime.UtcNow.AddHours(1))
+    .AddJob<CronJob>(name: nameof(CronJob));
 
 var app = builder.Build();
 
